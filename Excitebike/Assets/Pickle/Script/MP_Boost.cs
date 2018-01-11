62fc385c43e4886a3c5c92747ce314273389db1b
Pickle3063 <35244336+Pickle3063@users.noreply.github.com>
2018-01-10 21:06:25 -0500

    Add files via upload

---
+++ Excitebike/Assets/Pickle/Script/MP_Boost.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MP_Boost : MonoBehaviour {
+
+    float speed;
+    bool boosting;
+    GameObject floor;
+    [SerializeField] float maxSpeed;
+    float minSpeed = 0;
+
+	// Use this for initialization
+	void Start () {
+        floor = GameObject.Find("floor");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Boost()
+    {
+        print("Boost");
+        boosting = true;
+        floor.transform.Translate(Vector2.left * (speed += (Time.deltaTime * 2)));
+
+        if(speed > maxSpeed)
+        {
+            speed = maxSpeed;
+        }
+    }
+
+    public void StopBoost()
+    {
+        if (boosting)
+        {
+            floor.transform.Translate(Vector2.left * (speed -= Time.deltaTime * .5f));
+            if(speed < minSpeed)
+            {
+                speed = minSpeed;
+            }
+        }
+    }
+}
