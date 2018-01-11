0091db1bbb33f43337eb9bccb7f9bc6df99f7e23
Pickle3063 <35244336+Pickle3063@users.noreply.github.com>
2018-01-10 20:41:06 -0500

    Controls and Acceleration scripts

---
+++ Excitebike/Assets/Pickle/Pickle/Script/MP_Controls.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MP_Controls : MonoBehaviour {
+
+    MP_Acceleration ACC;
+
+	// Use this for initialization
+	void Start () {
+        ACC = gameObject.GetComponent<MP_Acceleration>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            print("A button pressed");
+
+            ACC.Accelerate();
+        }
+        else
+        {
+            ACC.Decelerate();
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            print("B button pressed");
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            print("UP Directional pressed");
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            print("DOWN Directional pressed");
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("LEFT Directional pressed");
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            print("RIGHT Directional pressed");
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("START button pressed");
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Quote))
+        {
+            print("SELECT button pressed");
+
+
+        }
+
+    }
+}
