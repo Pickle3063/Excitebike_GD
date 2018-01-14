5c275fcd7e9e26a9407089460d36822c2599739a
Pickle3063 <35244336+Pickle3063@users.noreply.github.com>
2018-01-14 18:03:14 -0500

    uploading progress

---
+++ Excitebike/Assets/Pickle/Script/MP_GroundControl.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class MP_GroundControl : MonoBehaviour {
+
+    enum WheelieStates
+    {
+        IDLE,
+        WHEELIE,
+        READYTOCRASH,
+        CRASH,
+
+        NUM_STATES
+    }
+
+    WheelieStates curState = WheelieStates.IDLE;
+    Dictionary<WheelieStates, Action> fsm = new Dictionary<WheelieStates, Action>();
+
+    Quaternion curRot;
+    Quaternion newRot;
+
+	// Use this for initialization
+	void Start () {
+        fsm.Add(WheelieStates.IDLE, new Action(StateIdle));
+        fsm.Add(WheelieStates.WHEELIE, new Action(StateWheelie));
+        fsm.Add(WheelieStates.READYTOCRASH, new Action(StateReadyToCrash));
+        fsm.Add(WheelieStates.CRASH, new Action(StateCrash));
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        fsm[curState].Invoke();
+
+	}
+
+    public void IncreaseRot()
+    {
+        curRot.Set(0f, 0f, 0f, 0f);
+        newRot.Set(0f, 0f, 54f, 0f);
+        Quaternion.RotateTowards(curRot, newRot, 2 * Time.deltaTime);
+    }
+    
+    public void DecreaseRot()
+    {
+
+    }
+
+    void StateIdle()
+    {
+
+    }
+
+    void StateWheelie()
+    {
+
+    }
+
+    void StateReadyToCrash()
+    {
+
+    }
+
+    void StateCrash()
+    {
+
+    }
+
+    void SetState(WheelieStates newState)
+    {
+        curState = newState;
+    }
+}
