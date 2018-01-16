f5248dd421fbf98487c4d2d9b35bd6cf8d98e85a
Pickle3063 <35244336+Pickle3063@users.noreply.github.com>
2018-01-15 21:49:05 -0500

    Adding crashing for wheelie

--- Excitebike/Assets/Pickle/Script/MP_ChangeLanes.cs
+++ Excitebike/Assets/Pickle/Script/MP_ChangeLanes.cs
@@ -11,7 +11,8 @@ public class MP_ChangeLanes : MonoBehaviour {
         ONE,//bottom lane
         TWO,//second lane up
         THREE,//third lane up
-        FOUR//fourth lane up
+        FOUR,//fourth lane up
+        RECLANE//where the player goes after crashing or overheating
     }
 
     //sets current state as lane three(second from the top)
@@ -19,29 +20,49 @@ public class MP_ChangeLanes : MonoBehaviour {
     Dictionary<LaneStates, Action> fsm = new Dictionary<LaneStates, Action>();
 
     //the heights of the four different lanes
-    float heightOne = -1.4f;
-    float heightTwo = -0.4f;
-    float heightThree = 0.6f;
-    float heightFour = 1.6f;
+    [SerializeField] float heightOne = -1.4f;
+    [SerializeField] float heightTwo = -0.4f;
+    [SerializeField] float heightThree = 0.6f;
+    [SerializeField] float heightFour = 1.6f;
+    [SerializeField] float heightRec = 2.6f;
+
+    //speed at which the player changes lanes
+    [SerializeField] float speed;
+
     //used to change the player's height(changing lanes)
     Vector3 curPos;
 
     //stops the state change from being called back to back
-    float timer = 0.2f;
-    float timePassed;
+    [SerializeField] float timer = 0.2f;
+    [SerializeField] float timePassed;
+    //secondary timer for when you crash
+    [SerializeField] float crashTimer;
 
-	// Use this for initialization
-	void Start () {
+    //checks if the player needs to go to the lane above four for overheating or crashing
+    bool needTopLane = false;
+
+    //accessing scripts
+    MP_Crash Crashing;
+    MP_Controls Controls;
+    MP_Acceleration Accel;
+    MP_GroundControl GroundCon;
+
+    // Use this for initialization
+    void Start() {
         //adding the four states to the dictionary
         fsm.Add(LaneStates.ONE, new Action(StateOne));
         fsm.Add(LaneStates.TWO, new Action(StateTwo));
         fsm.Add(LaneStates.THREE, new Action(StateThree));
         fsm.Add(LaneStates.FOUR, new Action(StateFour));
+        fsm.Add(LaneStates.RECLANE, new Action(StateRec));
         SetState(LaneStates.ONE);
 
         curPos = transform.position;
 
-        
+        Crashing = GetComponent<MP_Crash>();
+        Controls = GetComponent<MP_Controls>();
+        Accel = GetComponent<MP_Acceleration>();
+        GroundCon = GetComponent<MP_GroundControl>();
     }
 
     // Update is called once per frame
@@ -49,10 +70,54 @@ public class MP_ChangeLanes : MonoBehaviour {
         //continues to call current state every frame
         fsm[curState].Invoke();
         //sets the current position every frame
-        transform.SetPositionAndRotation(curPos, transform.rotation);
+        //transform.SetPositionAndRotation(curPos, transform.rotation);
+
+        transform.position = Vector3.Lerp(transform.position, curPos, speed * Time.deltaTime);
 
         //timer
         timePassed += Time.deltaTime;
+
+        //checks if player has crashed OR overheated
+        if (needTopLane)
+        {
+            //slowly moves player to the recovery lane
+            if (timePassed > timer && curState != LaneStates.RECLANE)
+            {
+                //disables player input, it needs both disabled for some reason
+                Controls.enabled = false;
+                Accel.enabled = false;
+                
+                //looping timer and state change
+                timePassed = 0;
+                curState++;
+                
+            }
+        }
+        
+    }
+
+    //used to set the timer to the crash time for a wheelie
+    public void WheelieCrash()
+    {
+        crashTimer = GroundCon.GetWheelieTime();
+    }
+
+    //used by MP_Crash to speed up the timer during a crash
+    public void SpeedUpCrashTime()
+    {
+        timePassed = timePassed + Crashing.GetSpeedUpTime();
+    }
+
+    //toggles boolean
+    public void ChangeNeedTopLane()
+    {
+        needTopLane = !needTopLane;
+    }
+
+    //for checking true or false in other scripts
+    public bool GetNeedTopLane()
+    {
+        return needTopLane;
     }
 
     //called in controls, moves the player up a lane
@@ -106,6 +171,33 @@ public class MP_ChangeLanes : MonoBehaviour {
     {
         curPos = new Vector3(-32.984f, heightFour, -2);
       
+    }
+
+    void StateRec()
+    {
+        curPos = new Vector3(-32.984f, heightRec, -2);
+
+        //insert if check for overheating
+        /*if(isOverheating){
+         * if(!overheating{
+         * curState = LaneStates.FOUR;
+         * }
+         * }
+         */
+        //checking if the reason for going to this state if for crashing
+        if (Crashing.GetIsCrashed())
+        {
+            //keeps player in recovery lane for set amount of time
+            if(timePassed > crashTimer)
+            {
+                //resets timer, toggles isCrashed, changes lane to the top lane, reenables input
+                timePassed = 0;
+                Crashing.ChangeIsCrashed();
+                curState = LaneStates.FOUR;
+                Controls.enabled = true;
+                Accel.enabled = true;
+            }
+        }
     }
    
     void SetState(LaneStates newState)
--- Excitebike/Assets/Pickle/Script/MP_GroundControl.cs
+++ Excitebike/Assets/Pickle/Script/MP_GroundControl.cs
@@ -13,14 +13,21 @@ public class MP_GroundControl : MonoBehaviour {
     Quaternion curRot;
     //speed the player rotates
     [SerializeField] float Speed;
+    //how long the player is in the recovery lane for a wheelie crash
+    [SerializeField] float WheelieCrashTimer;
     //used to find the number equal to a specific rotation for use later
     [SerializeField] float curZRot;
-    
+
+    //accessing crash script
+    MP_Crash Crashing;
+    MP_ChangeLanes LaneSwitch;
+
 	// Use this for initialization
 	void Start() {
         //sets starting rotation for later use
          StartRot = transform.rotation;
-
+        Crashing = GetComponent<MP_Crash>();
+        LaneSwitch = GetComponent<MP_ChangeLanes>();
 	}
 	
 	// Update is called once per frame
@@ -55,6 +62,12 @@ public class MP_GroundControl : MonoBehaviour {
         }
     }
 
+    //for resetting rotation slowly
+    public void ResetRot()
+    {
+        transform.rotation = Quaternion.Lerp(curRot, StartRot, Speed * Time.deltaTime);
+    }
+
     //used for when crashing is implemented
     //uses the current Z rotation versus the number you get from curZRot at specific rotations
     void CrashCheck()
@@ -62,14 +75,27 @@ public class MP_GroundControl : MonoBehaviour {
         //checks to see if you're in that zone in which the player wobbles
         if(curRot.z > 0.4539904)//0.4539904 = 54f
         {
-            print("Crashing");
+            //do animation stuff here i guess
+            //sets the crash timer in MP_ChangeLanes to the crash timer for a wheelie
+            LaneSwitch.WheelieCrash();
         }
 
         //checks to see if player has crashed
         if(curRot.z > 0.5735764)//0.5735764 = 70f
         {
-            print("Crashed");
+            //calls a function that sets the current crash timer to the wheelie crash timer
+            LaneSwitch.WheelieCrash();
+            //changing boolean that checks if you are crashing and starts rotation
+            Crashing.ChangeIsCrashed();
+            ResetRot();
+            
         }
        
+    }
+
+    //the wheelie crash timer accessible by other scripts
+    public float GetWheelieTime()
+    {
+        return WheelieCrashTimer;
     }
 }
