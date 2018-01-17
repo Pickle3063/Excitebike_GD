bac83c05ec1669bab60b913f55687b873b4d0481
MagicalClimax <32495561+MagicalClimax@users.noreply.github.com>
2018-01-17 14:10:04 -0800

    Add files via upload
    Menu Addition

---
+++ MenuStuff/MenuStuff.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MenuStuff : MonoBehaviour {
+
+    public Sprite StartMenu1;
+    public Sprite StartMenu2;
+    public Sprite StartMenu3;
+    public Sprite StartMenuNull;
+
+    public Sprite SoloMenu1;
+    public Sprite SoloMenu2;
+    public Sprite SoloMenu3;
+    public Sprite SoloMenu4;
+    public Sprite SoloMenu5;
+    public Sprite SoloMenuNull;
+
+    public Sprite DesignMenu1;
+    public Sprite DesignMenu2;
+    public Sprite DesignMenu3;
+    public Sprite DesignMenu4;
+    public Sprite DesignMenu5;
+    public Sprite DesignMenu6;
+
+    public AudioClip[] TitleTrack = new AudioClip[4];
+    public AudioClip BeepSelect;
+    public AudioClip RaceIntro;
+
+
+    //This Script is for storing Menu Sprites.
+	
+	void Start () {
+        //GameObject.Find("MenuImage").GetComponent<Image>().sprite = StartMenuNull;
+
+    }
+	
+	
+	void Update () {
+		
+	}
+}
