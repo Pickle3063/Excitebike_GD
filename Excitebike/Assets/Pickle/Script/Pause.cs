658f3a834b5f7bcc711d27b59ab45e3a20666f53
Pickle3063 <35244336+Pickle3063@users.noreply.github.com>
2018-01-23 15:21:10 -0500

    uploading changes before merge

--- Excitebike/Assets/Pickle/Script/Pause.cs
+++ Excitebike/Assets/Pickle/Script/Pause.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Pause : MonoBehaviour {
 
-    public MP_Acceleration accel;
+    public MP_Boost_Accel accel;
     public MP_Controls control;
-    private bool pause;
+    private bool pause = false;
 
 	// Use this for initialization
 	void Start () {
-        accel = GetComponent<MP_Acceleration>();
+        accel = GetComponent<MP_Boost_Accel>();
         control = GetComponent<MP_Controls>();
     }
 
@@ -21,14 +21,15 @@ public class Pause : MonoBehaviour {
         if (pause && Input.GetKeyDown(KeyCode.Return))
         {
             pause = false;
-            control.enabled = false;
-            accel.enabled = false;
+            control.enabled = true;
+            accel.enabled = true;
+            
         }
         else if (Input.GetKeyDown(KeyCode.Return) && !pause)
         {
             pause = true;
-            control.enabled = true;
-            accel.enabled = true;
+            control.enabled = false;
+            accel.enabled = false;
         }
 
     }
